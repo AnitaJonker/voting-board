af7a2c9b55d31acffe395671ad8f9bafaf3248d1
AnitaJonker <anitaroneljonker@gmail.com>
2026-06-03 10:50:37 +0200

    Update vote counters

--- backend-full/ideas/serializers.py
+++ backend-full/ideas/serializers.py
@@ -4,10 +4,22 @@ from .models import Idea
 
 class IdeaSerializer(serializers.ModelSerializer):
     vote_count = serializers.SerializerMethodField()
+    has_voted = serializers.SerializerMethodField()
+    yes_votes = serializers.SerializerMethodField()
+    no_votes = serializers.SerializerMethodField()
 
     class Meta:
         model = Idea
-        fields = ["id", "title", "description", "created_at", "vote_count"]
+        fields = [
+            "id",
+            "title",
+            "description",
+            "created_at",
+            "vote_count",
+            "has_voted",
+            "yes_votes",
+            "no_votes",
+        ]
 
     def get_vote_count(self, obj):
         return obj.votes.count()
@@ -15,3 +27,9 @@ class IdeaSerializer(serializers.ModelSerializer):
     def get_has_voted(self, obj):
         user = self.context["request"].user
         return obj.votes.filter(user=user).exists()
+
+    def get_yes_votes(self, obj):
+        return obj.votes.filter(choice="Y").count()
+
+    def get_no_votes(self, obj):
+        return obj.votes.filter(choice="N").count()
