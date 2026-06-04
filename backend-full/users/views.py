3d21c9403d8dafbc4a445ec535130a8976ff53b3
AnitaJonker <anitaroneljonker@gmail.com>
2026-06-04 10:18:52 +0200

    remove unnessary code

--- backend-full/users/views.py
+++ backend-full/users/views.py
@@ -1,17 +1,16 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny
 from django.contrib.auth import authenticate
 
 
 class LoginView(APIView):
 
     authentication_classes = []
-    permission_classes = []
+    permission_classes = [AllowAny]
 
     def post(self, request):
         username = request.data.get("username", "").strip()
