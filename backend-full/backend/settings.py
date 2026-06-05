a0cab097071dd5983380916926a30a9d5e2aca3e
AnitaJonker <anitaroneljonker@gmail.com>
2026-06-05 07:31:24 +0200

    add secret key if no .env found

--- backend-full/backend/settings.py
+++ backend-full/backend/settings.py
@@ -12,8 +12,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+
+# https://djecrety.ir/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = (
+    os.getenv("SECRET_KEY") or "i9d__02v)u-!4zm-p0dcfz%*9sh%x_&$90@=!(-%t^mn6sq&qy"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
